05094a378a36afb15bee0e58fab08cbef304828a
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2022-05-14 12:27:45 +0800

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,9 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
+// Import alert
+import Swal from 'sweetalert2'
+
 function Home() {
   const [text, setText] = useState("");
   const [isReady, setIsReady] = useState(false);
@@ -9,21 +12,58 @@ function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          hidden={!isReady}
-          src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
-          className="App-logo"
-          alt="logo"
-          style={{ padding: "10px" }}
-        />
-        <b>
+        <NavLink to='/pokedex'>
+          <img
+            hidden={!isReady}
+            src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
+            className="App-logo"
+            alt="logo"
+            style={{ padding: "10px" }}
+          />
+        </NavLink>
+
+        {/* Commented out the requirement */}
+        {/* <b>
           Requirement: Try to show the hidden image and make it clickable that
           goes to /pokedex when the input below is "Ready!" remember to hide the
           red text away when "Ready!" is in the textbox.
-        </b>
-        <p>Are you ready to be a pokemon master?</p>
-        <input type="text" name="name" />
-        <span style={{ color: "red" }}>I am not ready yet!</span>
+        </b> */}
+        <h2>Are you ready to be a pokemon master?</h2>
+        <input 
+          type="text" 
+          name="name"
+          onChange={(e) => {
+            let text = e.target.value
+          
+            // Conditional statements to check for text input 
+          
+            // if (text === 'Ready!' || 'ready!' || 'READY!')
+            //   setIsReady(true)
+          
+            // else if (text === 'Ready' || 'ready' || 'READY') {
+            //   setIsReady(false)
+            //   // If the user miss the <!> sign  
+            //   Swal.fire(
+            //     'Almost there!',
+            //     'You miss something',
+            //     'info'
+            //   )
+            // }
+
+            // else
+            //   return setIsReady(false)
+
+            // Conditional statements for inline if else (conditional rendering)
+            // Source: https://stackoverflow.com/questions/40477245/is-it-possible-to-use-if-else-statement-in-react-render-function
+
+            // Show the hidden image and make it clickable that goes to /pokedex when the input below is "Ready!"
+            text == ("Ready!" || "ready!" || "READY!")  ? setIsReady(true) : setIsReady(false)
+          }}
+        />
+        <br/>
+        
+        {/* Hide the red text away when "Ready!" is in the textbox */}
+        { (isReady) ? (null && undefined) : <span style={{ color: "red" }}>I am not ready yet!</span>}
       </header>
     </div>
   );
